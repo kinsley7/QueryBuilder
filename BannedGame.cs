d493a7fb48514e7925df0e715dd068387cca294b
kinsley crowdis <113950546+kinsley7@users.noreply.github.com>
2023-09-21 15:13:53 -0400

    Add files via upload

---
+++ BannedGame.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**       
+ *--------------------------------------------------------------------
+ * 	   File name: BannedGames
+ * 	Project name:CrowdisLab3
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 kinsley crowdis crowdis@etsu.edu			
+ *          Course-Section: CSCI 2150-800
+ *           Creation Date:	09/14/2023
+ * -------------------------------------------------------------------
+ */
+
+namespace CrowdisLab3
+{
+    internal class BannedGame : IClassModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Series { get; set; }
+        public string Country { get; set; }
+        public string? Details { get; set; }
+
+        public BannedGame() { }
+        public BannedGame(int id, string title, string series, string country, string? details)
+        {
+            Id = id;
+            Title = title;
+            Series = series;
+            Country = country;
+            Details = details;
+        }
+
+        public override string ToString()
+        {
+            string msg = "";
+
+            msg += $"Id: {Id}\n";
+            msg += $"Title: {Title}\n";
+            msg += $"Series: {Series} \n";
+            msg += $"Country: {Country} \n";
+            msg += $"Details: {Details} \n";
+
+            return msg;
+        }
+    }
+}
---
+++ Pokemon.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+/**       
+ *--------------------------------------------------------------------
+ * 	   File name: Pokemon
+ * 	Project name:CrowdisLab3
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 kinsley crowdis crowdis@etsu.edu			
+ *          Course-Section: CSCI 2150-800
+ *           Creation Date:	09/14/2023
+ * -------------------------------------------------------------------
+ */
+
+namespace CrowdisLab3
+{
+    internal class Pokemon : IClassModel
+    {
+        public int Id { get; set; }
+        public int DexNumber { get; set; }
+        public string Name { get; set; }
+        public string? Form { get; set; }
+        public string Type1 { get; set; }
+        public string? Type2 { get; set; }
+        public int Total { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+        public int Speed { get; set; }
+        public int Generation { get; set; }
+
+        public Pokemon() { }
+        public Pokemon(int id, int dexNumber, string name, string? form, string type1,
+        string type2, int total, int hp, int attack, int defense, int specialAttack,
+        int specialDefense, int speed, int generation)
+        {
+            this.Id = id;
+            this.DexNumber = dexNumber;
+            this.Name = name;
+            this.Form = form;
+            this.Type1 = type1;
+            this.Type2 = type2;
+            this.Total = total;
+            this.HP = hp;
+            this.Attack = attack;
+            this.Defense = defense;
+            this.SpecialAttack = specialAttack;
+            this.SpecialDefense = specialDefense;
+            this.Speed = speed;
+            this.Generation = generation;
+        }
+
+        // tostring
+
+        public override string ToString()
+        {
+            string msg = "";
+
+            msg += $"Id: {Id}  \n";
+            msg += $"Dex Number: {DexNumber}  \n";
+            msg += $"Name: {Name}  \n";
+            msg += $"Form: {Form}  \n";
+            msg += $"Type 1: {Type1}  \n";
+            msg += $"Type 2: {Type2}  \n";
+            msg += $"Total: {Total}  \n";
+            msg += $"Hit Points: {HP}  \n";
+            msg += $"Attack: {Attack}  \n";
+            msg += $"Defense: {Defense}  \n";
+            msg += $"Special Attack: {SpecialAttack}  \n";
+            msg += $"Special Defense: {SpecialDefense}  \n";
+            msg += $"Speed: {Speed}  \n";
+            msg += $"Generation: {Generation}  \n";
+
+            return msg;
+        }
+    }
+
+}
