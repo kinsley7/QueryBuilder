082e9ac81bdae290a1fbde1240f3992b2766b9a9
kinsley crowdis <113950546+kinsley7@users.noreply.github.com>
2023-09-21 20:36:06 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,228 @@
+﻿
+
+using System.Xml.Schema;
+/**       
+*--------------------------------------------------------------------
+* 	   File name:Program
+* 	Project name:CrowdisLab3
+*--------------------------------------------------------------------
+* Author’s name and email:	 kinsley crowdis crowdis@etsu.edu			
+*          Course-Section: CSCI 2150-800
+*           Creation Date:	09/14/2023
+* -------------------------------------------------------------------
+*/
+namespace CrowdisLab3
+{
+    internal class Program
+    {
+        static string projectRootFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+        static string dbPath = projectRootFolder + "\\data\\data.db";
+
+
+        static void Main(string[] args)
+        {
+            QueryBuilder stuff = new QueryBuilder(dbPath);
+            Pokemon pokemon = new Pokemon();
+            BannedGame bannedGame = new BannedGame();
+
+
+            ///<summary>
+            ///Write all objects in a collection to the database (create)
+            ///</summary>
+            ///
+
+            var filePath = projectRootFolder + "\\data\\AllPokemon.csv";
+            using (var sr = new StreamReader(filePath))
+            {
+
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var data = line.Split(',');
+
+                    Pokemon p = new Pokemon();
+
+                    p.DexNumber = int.Parse(data[0]);
+                    p.Name = data[1];
+                    p.Form = data[2];
+                    p.Type1 = data[3];
+                    p.Type2 = data[4];
+                    p.Total = int.Parse(data[5]);
+                    p.HP = int.Parse(data[6]);
+                    p.Attack = int.Parse(data[7]);
+                    p.Defense = int.Parse(data[8]);
+                    p.SpecialAttack = int.Parse(data[9]);
+                    p.SpecialDefense = int.Parse(data[10]);
+                    p.Speed = int.Parse(data[11]);
+                    p.Generation = int.Parse(data[12]);
+
+
+
+
+
+                    stuff.Create<Pokemon>(p);
+                }
+
+
+                List<Pokemon> dataList = stuff.ReadAll<Pokemon>();
+                Console.WriteLine("Pokemon Table: ");
+                foreach (var p in dataList)
+                    Console.WriteLine(p);
+
+                var filePathTwo = projectRootFolder + "\\data\\BannedGames.csv";
+                using (var sr2 = new StreamReader(filePathTwo))
+                {
+                    sr2.ReadLine();
+
+                    while (!sr2.EndOfStream)
+                    {
+                        var line = sr2.ReadLine();
+                        var data = line.Split(',');
+
+                        BannedGame bg = new BannedGame();
+                        bg.Title = data[0];
+                        bg.Series = data[1];
+                        bg.Country = data[2];
+                        bg.Details = data[3];
+
+
+                        stuff.Create<BannedGame>(bg);
+                    }
+
+                    List<BannedGame> bannedGames = stuff.ReadAll<BannedGame>();
+                    Console.WriteLine("Banned Games Table: ");
+                    foreach (var item in bannedGames)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    Console.WriteLine("Write all objects in a collection to the database ✓");
+
+
+                    ///<summary>
+                    ///Write a single object to the database (create)
+                    ///</summary>
+                    Console.Write("Would you like to add 'pokemon' or a 'banned game' to the database? ");
+                    string userinput = (Console.ReadLine().Trim().ToLower()).Substring(0, 1);
+                create:
+                    switch (userinput)
+                    {
+                        case "p":
+                            Pokemon userPokemon = new Pokemon();
+                            Console.WriteLine("Creating New Pokemon...");
+                            Console.WriteLine("--------------------------------------");
+                            Console.WriteLine("Enter the following information...");
+
+                            //id gets auto added
+
+                            Console.Write("Dex Number: ");
+                            userPokemon.DexNumber = int.Parse(Console.ReadLine());
+
+                            Console.Write("Name of Pokemon: ");
+                            userPokemon.Name = Console.ReadLine();
+
+                            Console.Write("Form: ");
+                            userPokemon.Form = Console.ReadLine();
+
+                            Console.Write("Type 1: ");
+                            userPokemon.Type1 = Console.ReadLine();
+
+                            Console.Write("Type 2: ");
+                            userPokemon.Type2 = Console.ReadLine();
+
+                            Console.Write("Hit Points: ");
+                            userPokemon.HP = int.Parse(Console.ReadLine());
+
+                            Console.Write("Attack: ");
+                            userPokemon.Attack = int.Parse(Console.ReadLine());
+
+                            Console.Write("Defense: ");
+                            userPokemon.Defense = int.Parse(Console.ReadLine());
+
+                            Console.Write("Special Attack: ");
+                            userPokemon.SpecialAttack = int.Parse(Console.ReadLine());
+
+                            Console.Write("Special Defense: ");
+                            userPokemon.SpecialDefense = int.Parse(Console.ReadLine());
+
+                            Console.Write("Speed: ");
+                            userPokemon.Speed = int.Parse(Console.ReadLine());
+
+                            Console.Write("Generation: ");
+                            userPokemon.Generation = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Calculating 'Total'...");
+
+                            userPokemon.Total = userPokemon.HP + userPokemon.Attack + userPokemon.Defense + userPokemon.SpecialAttack + userPokemon.SpecialDefense + userPokemon.Speed;
+
+                            Console.WriteLine($"'Total' value is: {userPokemon.Total}");
+
+
+                            stuff.Create(userPokemon);
+                            
+
+                            List<Pokemon> dataListNew = stuff.ReadAll<Pokemon>();
+                            Console.WriteLine("Click Enter to view new Pokemon Table: ");
+                            Console.ReadLine();
+                            foreach (var item in dataListNew)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+
+
+                        case "b":
+
+                            BannedGame userBannedGame = new BannedGame();
+                            Console.WriteLine("Creating New Banned Game...");
+                            Console.WriteLine("--------------------------------------");
+                            Console.WriteLine("Enter the following information...");
+
+                            Console.Write("Title of Game: ");
+                            userBannedGame.Title = Console.ReadLine();
+
+                            Console.Write("Name of Series: ");
+                            userBannedGame.Series = Console.ReadLine();
+
+                            Console.Write("Country Game is Banned in: ");
+                            userBannedGame.Country = Console.ReadLine();
+
+                            Console.Write("Details about Ban: ");
+                            userBannedGame.Details = Console.ReadLine();
+
+
+                            stuff.Create(userBannedGame);
+
+                            List<BannedGame> bannedGamesNew = stuff.ReadAll<BannedGame>();
+                            Console.WriteLine("Click enter to view new Banned Games table: ");
+                            Console.ReadLine();
+                            foreach (var item in bannedGamesNew)
+                            {
+                                Console.WriteLine(item);
+                            }
+
+                            break;
+
+                        default:
+                            Console.Write("Please enter 'pokemon' or 'banned game': ");
+                            userinput = (Console.ReadLine().Trim().ToLower()).Substring(0, 1);
+                            goto create;
+                    }
+                    Console.WriteLine("Single object written to the database ✓");
+
+                    ///<summary>
+                    ///erase all records (deleteall)
+                    ///</summary>
+                    Console.WriteLine("Clearing Database...");
+                    stuff.DeleteAll();
+                    Console.WriteLine("Database Erased ✓");
+
+                    Console.WriteLine("Thanks for using :)");
+
+
+
+                }
+            }
+        }
+    }
+}
---
+++ QueryBuilder.cs
@@ -0,0 +1,305 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+/**       
+ *--------------------------------------------------------------------
+ * 	   File name: QueryBuilder
+ * 	Project name:CrowdisLab3
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 kinsley crowdis crowdis@etsu.edu			
+ *          Course-Section: CSCI 2150-800
+ *           Creation Date:	09/14/2023
+ * -------------------------------------------------------------------
+ */
+
+namespace CrowdisLab3
+{
+    public class QueryBuilder : IDisposable
+    {
+        SqliteConnection? connection;
+
+        //opens everytime this class is used instad of opening and closing for every method
+        public QueryBuilder(string dbPath)
+        {
+            connection = new SqliteConnection($"Data Source={dbPath}");
+            connection.Open();
+        }
+
+        /// <summary>
+        /// Read command to retrieve a single record given a property called 'Id'
+        /// - Data types that are in the DateTime format, SQLite has to store them in YYYY-MM-DD format
+        /// - If not, null will be stored
+        /// </summary>
+        /// <param name="id">The Id value of the object's corresponding DB record</param>
+        /// <returns>An object of type T</returns>
+        public T Read<T>(int id) where T : new()
+        {
+            var command = connection!.CreateCommand();
+            command.CommandText = $"SELECT * FROM {typeof(T).Name} where Id = {id}";
+
+            var reader = command.ExecuteReader();
+
+            T data = new T();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    // convert integer data to int data type from SQLite's int64 default
+                    if (typeof(T).GetProperty(reader.GetName(i)).PropertyType == typeof(int))
+                    {
+                        typeof(T).GetProperty(reader.GetName(i)).SetValue(data, Convert.ToInt32(reader.GetValue(i)));
+                    }
+                    // if datetime, format the string correctly
+                    else if (typeof(T).GetProperty(reader.GetName(i)).PropertyType == typeof(DateTime) && reader.GetValue(i).ToString().Split('-').Length == 3)
+                    {
+                        string[] dateString = reader.GetValue(i).ToString().Split('-');
+                        int[] dateNum = new int[3];
+                        for (int k = 0; k < 3; k++)
+                        {
+                            dateNum[k] = Convert.ToInt32(dateString[k]);
+                        }
+                        var dateTime = new DateTime(dateNum[0], dateNum[1], dateNum[2]);
+                        typeof(T).GetProperty(reader.GetName(i)).SetValue(data, dateTime);
+                    }
+                    // other data types
+                    else
+                    {
+                        typeof(T).GetProperty(reader.GetName(i)).SetValue(data, reader.GetValue(i));
+                    }
+                }
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// ReadAll operation to retrieve all records from a SQLite table
+        /// The same data type concerns from Read apply here, as well
+        /// </summary>
+        /// <returns>A list of T objects</returns>
+        public List<T> ReadAll<T>() where T : new() //has a default constructor
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = $"SELECT * FROM {typeof(T).Name}";
+            var reader = command.ExecuteReader();
+
+            T data;
+            var dataList = new List<T>();
+
+            while (reader.Read())
+            {
+                data = new T();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    // same code as the Read
+                    int id;
+                    
+                    // convert integer data to int data type from SQLite's int64 default
+                    if (typeof(T).GetProperty(reader.GetName(i)).PropertyType == typeof(int))
+                    {
+                        typeof(T).GetProperty(reader.GetName(i)).SetValue(data, Convert.ToInt32(reader.GetValue(i)));
+                    }
+                    // if datetime, format the string correctly
+                    else if (typeof(T).GetProperty(reader.GetName(i)).PropertyType == typeof(DateTime) && reader.GetValue(i).ToString().Split('-').Length == 3)
+                    {
+                        string[] dateString = reader.GetValue(i).ToString().Split('-');
+                        int[] dateNum = new int[3];
+                        for (int k = 0; k < 3; k++)
+                        {
+                            dateNum[k] = Convert.ToInt32(dateString[k]);
+                        }
+                        var dateTime = new DateTime(dateNum[0], dateNum[1], dateNum[2]);
+                        typeof(T).GetProperty(reader.GetName(i)).SetValue(data, dateTime);
+                    }
+                    // other data types
+                    else
+                    {
+                        typeof(T).GetProperty(reader.GetName(i)).SetValue(data, reader.GetValue(i));
+                    }
+                }
+                dataList.Add(data);
+            }
+            return dataList;
+        }
+
+        ///<summary>
+        ///the create method
+        ///</summary>
+        // TODO: Add the Create method
+        public void Create<T>(T obj) where T : IClassModel
+        {
+
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            // get property values
+            var values = new List<string>();
+            var names = new List<string>();
+            PropertyInfo property;
+
+            for (int i = 1; i < properties.Length; i++)
+            {
+                property = properties[i];
+                // format DateTime for DB (reverse the process in Read)
+                // quotation marks are necessary for the date format
+                if (property.PropertyType == typeof(DateTime))
+                {
+                    values.Add($"\"{((DateTime)property.GetValue(obj)).Year}-{((DateTime)property.GetValue(obj)).Month}-{((DateTime)property.GetValue(obj)).Day}\"");
+                }
+                // format string for DB
+                // quotation marks are necessary for the text format
+                else if (property.PropertyType == typeof(string))
+                {
+                    values.Add($"\"{property.GetValue(obj).ToString()}\"");
+                }
+                // format other data types for DB
+                else
+                {
+                    values.Add(property.GetValue(obj).ToString());
+                }
+                names.Add(property.Name);
+            }
+
+
+            // build the insert statement
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"INSERT INTO [{typeof(T).Name}] ("); //opens the statement (with help from chatgpt)
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (i == values.Count - 1)
+                {
+                    sb.Append($"{names[i]}) ");
+                }
+                else
+                {
+                    sb.Append($"{names[i]}, ");
+                }
+            }
+
+            sb.Append("VALUES (");
+            for (int i = 0; i < values.Count; i++)  //just like the loop from the update method this makes sure the , goes in after each item and that it doesnt matter how many there are.
+            {
+                if (i == values.Count - 1)
+                {
+                    sb.Append($"{values[i]} ");
+                }
+                else
+                {
+                    sb.Append($"{values[i]}, ");
+                }
+            }
+
+            sb.Append(");"); //closes
+
+            // executes the insert statement
+            var command = connection.CreateCommand();
+            command.CommandText = sb.ToString();
+            var reader = command.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// Update operation to update a record in the SQLite database
+        /// </summary>
+        /// <param name="obj">Object of type T to be updated (its Id param will be used to find it)</param>
+        public void Update<T>(T obj) where T : IClassModel
+        {
+            // this code will be the same as Create, minus the SQL command
+            // get the property names of the object 
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            // get property values
+            var values = new List<string>();
+            var names = new List<string>();
+            PropertyInfo property;
+
+            for (int i = 1; i < properties.Length; i++)
+            {
+                property = properties[i];
+                // format DateTime for DB (reverse the process in Read)
+                // quotation marks are necessary for the date format
+                if (property.PropertyType == typeof(DateTime))
+                {
+                    values.Add($"\"{((DateTime)property.GetValue(obj)).Year}-{((DateTime)property.GetValue(obj)).Month}-{((DateTime)property.GetValue(obj)).Day}\"");
+                }
+                // format string for DB
+                // quotation marks are necessary for the text format
+                else if (property.PropertyType == typeof(string))
+                {
+                    values.Add($"\"{property.GetValue(obj).ToString()}\"");
+                }
+                // format other data types for DB
+                else
+                {
+                    values.Add(property.GetValue(obj).ToString());
+                }
+                names.Add(property.Name);
+            }
+
+            // build the insert statement
+            StringBuilder sb = new StringBuilder();
+
+            // include commas in between values / names UNLESS you're on the last item
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (i == values.Count - 1)
+                {
+                    sb.Append($"{names[i]} = {values[i]}");
+                }
+                else
+                {
+                    sb.Append($"{names[i]} = {values[i]}, ");
+                }
+            }
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"UPDATE {typeof(T).Name} SET {sb} WHERE Id = {obj.Id}";
+            var reader = command.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// Delete operation to remove the parameter object from the SQLite database
+        /// </summary>
+        /// <param name="obj">Object of type T to be deleted</param>
+        public void Delete<T>(T obj) where T : IClassModel
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = $"DELETE FROM {typeof(T).Name} WHERE Id = {obj.Id}";
+            var reader = command.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// Closes resources committed to reading SQLite file
+        /// (required for a 'using' block)
+        /// </summary>
+        public void Dispose()
+        {
+            connection!.Dispose();
+        }
+
+        //TODO: Create DeleteAll method
+        public void DeleteAll()
+        {
+            // help from bing ai. have to select all tables in order to drop them. (no GetSchema method :( )
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT name FROM [sqlite_master] WHERE type='table';"; //call on master table because it holds all info 🧙‍ mwhahaha
+            var reader = command.ExecuteReader();
+
+            //adds the table names to a list
+            List<string> tableNames = new List<string>();
+
+            while (reader.Read()) //create a loop that adds the names from the command to the list
+            {
+                tableNames.Add(reader.GetString(0));
+            }
+            reader.Close();
+
+            for (int i = 1; i < tableNames.Count; i++) //we have to skip the first table so that the master does not get deleted
+            {
+                command.CommandText = $"DELETE FROM {tableNames[i]};";
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
